20c582b6bb78527432e1a1dc8ca5760d7ba1e13d
Lucas VASSEUR <lvasseur@e2r9p9.42.fr>
2016-12-07 17:22:08 +0100

    bien

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 13:13:49 by lvasseur          #+#    #+#             */
-/*   Updated: 2016/12/07 13:21:50 by lvasseur         ###   ########.fr       */
+/*   Updated: 2016/12/07 17:18:39 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int				ft_number_of_pieces(char *str);
 char			**ft_tabdup(char **tab);
 char			*ft_delete_first(char *str);
 char			*ft_isole_pieces(char *str);
-char			**ft_morphing_to_piece(char **tab);
+char			**ft_morphing_to_piece(char **tab, int l);
 int				ft_is_k(char *str, char c);
 int				ft_is_k_fory(char **tab, char c, int y);
 char			**ft_malltab(int x, int y);
@@ -37,6 +37,8 @@ int				ft_is_that_ok(char **tab);
 int				max_x(char **tab);
 int				max_y(char **tab);
 int				ft_isvalid_all(char *str);
+int				lv_main(int ac, char **av);
+void			ft_tabfree(char **tab);
 
 typedef struct	s_coord
 {
