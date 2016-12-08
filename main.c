7a55805016ad4aa750023aad618e6abb3a580385
Lucas VASSEUR <lvasseur@e2r9p9.42.fr>
2016-12-08 14:22:47 +0100

    parfait

--- main.c
+++ main.c
@@ -6,21 +6,11 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 13:32:18 by lvasseur          #+#    #+#             */
-/*   Updated: 2016/12/08 13:25:49 by lvasseur         ###   ########.fr       */
+/*   Updated: 2016/12/08 14:22:35 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
-void	ft_tabstr(char **tab)
-{
-	ft_putstr("result:\n");
-	while (*tab)
-	{
-		ft_putstr(*tab++);
-		ft_putchar('\n');
-	}
-}
 
 int		main(int ac, char **av)
 {
@@ -36,10 +26,6 @@ int		main(int ac, char **av)
 		ft_putstr("error\n");
 		return (0);
 	}
-	while (truc)
-	{
-		ft_tabstr(truc->tab);
-		truc = truc->next;
-	}
+	ft_wow(truc);
 	return (0);
 }
