d9d4cad6e5b351442d35846af0f2330290621b19
Lucas VASSEUR <lvasseur@e3r11p8.42.fr>
2016-12-21 12:24:09 +0100

    push potentiellement final

--- ft_initcursor.c
+++ ft_initcursor.c
@@ -6,7 +6,7 @@
 /*   By: zadrien <zadrien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 03:09:04 by zadrien           #+#    #+#             */
-/*   Updated: 2016/12/14 03:09:24 by zadrien          ###   ########.fr       */
+/*   Updated: 2016/12/21 12:16:12 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 t_cursor	*ft_initcursor(t_cursor *lst)
 {
-	lst = (t_cursor*)malloc(sizeof(t_cursor));
+	if ((lst = (t_cursor*)malloc(sizeof(t_cursor))) == NULL)
+		return (NULL);
 	lst->x = 0;
 	lst->y = 0;
 	return (lst);
